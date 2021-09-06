d98fe3d6369fba0cef151e515c72a03e286f6590
meriem <boubakermeriem62@gmail.com>
2021-09-06 11:15:49 +0100

    Remove duplicated directory

--- Routers/userRouters.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const authmiddleware = require('../middlewares/authmiddleware')
-const router = express.Router()
-const {adduser} = require('../Controllers/addUserController')
-const {loginController} = require('../Controllers/LoginController')
-router.post('/registeruser',authmiddleware,adduser)
-router.post('/login',loginController)
-module.exports = router
